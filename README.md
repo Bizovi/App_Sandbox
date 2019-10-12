7236323b17ebaf1a31e9bd0a025ba9f4651ce66f
Bizovi <bizovim@gmail.com>
2019-10-13 02:42:25 +0300

    Setup users service and postgre db

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# App_Sandbox
-A sandbox for Flask, Dash, Shiny applications, APIs, Cloud architecture
+# Application Development Sandbox
+
+A playground for Flask, Dash, Shiny applications, APIs, Cloud architecture. Some elements of front-end, interactive data visualization, databases, DevOps.
+
+
+### Microservices with Docker, Flask, and React
+
+A course by [testdriven.io](https://testdriven.io) which fits perfectly with my current needs. Instead of deploying on AWS, I will be using Google Cloud Platform and corresponding services (e.g. Cloud Run, AppEngine, Endpoints).
---
+++ testdriven_app/services/users/project/__init__.py
@@ -0,0 +1,47 @@
+# services/users/project/__init__.py
+import os
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+
+
+# instantiate the app
+app = Flask(__name__)
+
+api = Api(app)
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    """
+    The database model for users
+    """
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+class UsersPing(Resource):
+    def get(self):
+        return {
+        'status': 'success',
+        'message': 'here we go again ...'
+    }
+
+
+# import sys   # for debugging purposes
+# print(app.config, file=sys.stderr)  # docker-compose logs
+
+api.add_resource(UsersPing, '/users/ping')
