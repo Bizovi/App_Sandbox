d602d9bba84644c35dcb6cacaa53b53a8613bdf3
Bizovi <bizovim@gmail.com>
2019-10-13 03:05:56 +0300

    API setup and Blueprints

--- testdriven_app/services/users/manage.py
+++ testdriven_app/services/users/manage.py
@@ -1,16 +1,33 @@
 # services/users/manage.py
 
 from flask.cli import FlaskGroup
-from project import app, db
 
+# packages for running the tests
+import sys
+import unittest
 
-cli = FlaskGroup(app)
+from project import create_app, db
+from project.api.models import User
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
 
 @cli.command('recreate_db')
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
 
 
 if __name__ == '__main__':
