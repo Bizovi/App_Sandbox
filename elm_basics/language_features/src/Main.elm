25dc96e9cb8c97df4d621673215444ca3396f39c
Bizovi <bizovim@gmail.com>
2020-03-15 19:23:13 -0400

    Example with temperature conversion and forms

--- elm_basics/language_features/src/Main.elm
+++ elm_basics/language_features/src/Main.elm
@@ -6,7 +6,7 @@
 -}
 module Main exposing (..)
 
-import Html exposing (Html, div, text, input, button, p, br)
+import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 import Browser
@@ -42,12 +42,24 @@ type Messages = -- choice type for messages
     | Reset
     | IncrementByTen
     | InputTextReversed String
+    | Name String
+    | Password String
+    | PasswordAgain String
+    | InputTemperature String
+
+
+type PasswordStatus = 
+    OK | TooShort | NotMatching
 
 -- Model <function> : Int -> Int -> Model
 type alias Model = 
     { value : Int
     , numberToAdd : Int
     , textString : String
+    , name : String
+    , password : String
+    , passwordAgain : String
+    , temperature : String
     }
 
 init : Model
@@ -55,6 +67,10 @@ init = -- initialize the model
     { value = 0
     , numberToAdd = 0
     , textString = ""
+    , name = ""
+    , password = ""
+    , passwordAgain = ""
+    , temperature = ""
     }
 
 {-
@@ -66,7 +82,7 @@ init = -- initialize the model
 parseNumber : String -> Int 
 parseNumber text =
     let
-        theMaybe = toInt text
+        theMaybe = toInt text -- used for error handling
     in
         case theMaybe of 
             Just val -> 
@@ -74,11 +90,10 @@ parseNumber text =
             Nothing -> 
                 0
 
+
 update : Messages -> Model -> Model
 update msg model =  -- item and value
     let -- used for debugging purposes at the beginning
-        a = 1
-        b = 2
         logmessage = log "msg" msg
         logmodel = log "model" model
     in
@@ -93,6 +108,14 @@ update msg model =  -- item and value
             { model | value = 0 }
         InputTextReversed txt -> 
             { model | textString = txt}
+        Name name -> 
+            { model | name = name}
+        Password pass -> 
+            { model | password = pass }
+        PasswordAgain pass -> 
+            { model | passwordAgain = pass}
+        InputTemperature temp ->
+            { model | temperature = temp }
 
 {-
     View function, tells Elm how to display stuff 
@@ -103,24 +126,93 @@ update msg model =  -- item and value
 -}
 view : Model -> Html Messages
 view model = 
-    div [] [
-          text (fromInt model.value)
+    div [] 
+        [ viewIncrement model -- Incrementing numbers and reset
+        , viewReverse model   -- Reverse a string and show length 
+        , div [] 
+            [ viewInput "text" "Name" model.name Name
+            , viewInput "password" "Password" model.password Password
+            , viewInput "password" "Confirm Password" model.passwordAgain PasswordAgain
+            , viewValidation model
+            ]
+        , viewConverter model
+        ] 
+
+
+viewIncrement : Model -> Html Messages
+viewIncrement model = 
+    div [] 
+        [ text (fromInt model.value)
         , text "\n"
         , div [] []
-          -- ==== Incrementing numbers and reset ====
         , input [ onInput ChangedAddText ] [] -- generate Msg when enters
         , button [ onClick Add ] [ text "Add" ]  -- generate Msg when clicks
         , button [ onClick IncrementByTen ] [ text "Increment by 10" ]
         , button [ onClick Reset ] [ text "Reset" ]
         , br [] []
-        , br [] []
-          -- ==== Reverse a string and show length ====
-        , div [] [
-              input [ placeholder "Text to reverse"
-                    , value model.textString
-                    , onInput InputTextReversed ] []
-            , div [] [ text (String.reverse model.textString) ]
-            , div [] [ text ( "Length: " ++ String.fromInt (String.length  model.textString)) ]
+        , hr [] []
         ]
-          -- ==== Some rudimentary forms ====
-    ] 
+
+
+viewReverse : Model -> Html Messages
+viewReverse model = 
+    div [] 
+        [ input [ placeholder "Text to reverse"
+                , value model.textString
+                , onInput InputTextReversed 
+                ] []
+        , div [] [ text (String.reverse model.textString) ]
+        , div [] [ text ( "Length: " ++ String.fromInt (String.length  model.textString)) ]
+        , br [] [] 
+        , hr [] []
+        ]
+
+
+viewConverter : Model -> Html Messages
+viewConverter model = 
+    case String.toFloat model.temperature of 
+        Just celsius ->
+            viewTempConverter model.temperature "blue" (String.fromFloat (celsius * 1.8 + 32))
+        Nothing ->
+            viewTempConverter model.temperature "red" "???"
+
+
+viewTempConverter : String -> String -> String -> Html Messages
+viewTempConverter temperature color farenheit = 
+    span []
+        [ br [] []
+        , br [] []
+        , input [ value temperature, onInput InputTemperature, style "width" "40px"] []
+        , text "°C = "
+        , span [ style "color" color ] [text farenheit]
+        , text "°F"
+        ]
+
+viewValidation : Model -> Html msg
+viewValidation model = 
+    let
+        status = passwordValidation model.password model.passwordAgain
+    in
+    case status of 
+        OK -> 
+            div [ style "color" "green" ] [ text "OK!" ]
+        TooShort -> 
+            div [ style "color" "red" ] [ text "Password too short. Min 9 chars" ]
+        NotMatching ->
+            div [ style "color" "red" ] [text "Passwords do not match!" ]
+
+
+passwordValidation : String -> String -> PasswordStatus
+passwordValidation password passwordAgain = 
+    if (password == passwordAgain) && 
+       ((String.length password) > 8) then
+        OK
+    else if (String.length password) <= 8 then
+        TooShort
+    else
+        NotMatching
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg = 
+    input [ type_ t, placeholder p, value v, onInput toMsg] []
